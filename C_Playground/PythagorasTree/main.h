1ba034cb2762dd70ace8067d875b894dccea9265
Lodomo <lodomo.dev@gmail.com>
2025-04-26 21:57:15 -0700

    Restructuring

--- C_Playground/PythagorasTree/main.h
+++
@@ -1,9 +0,0 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include "./utils/Utils.h"
-
-#define SCREEN_WIDTH 1000
-#define SCREEN_HEIGHT 1000
-
-#endif // MAIN_H
--- C_Playground/sqrt3over2/main.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    double result = sqrt(3.0) / 2.0;
-    printf("%.17g\n", result);
-    return 0;
-}
